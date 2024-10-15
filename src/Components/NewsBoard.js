7dfcbd6e8448a11bfc49d679bc54b13a8e16cd83
Harika Madhavapeddy <harikamadhavapeddy91@gmail.com>
2024-10-15 10:48:36 -0500

    new Url update

--- src/Components/NewsBoard.js
+++ src/Components/NewsBoard.js
@@ -8,15 +8,8 @@ export default function NewsBoard({category}) {
   //console.log(apiKey);
   useEffect(() => {
     console.log(category);
-    /*axios
-      .get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`)*/
-      axios.get("https://news-app-hazel-seven.vercel.app/news", {
-        params: {
-          country: "us",
-          category,
-          apiKey, // Use your News API key here
-        },
-      })
+    axios
+      .get(`https://newsappbackend-six.vercel.app/news?country=us&category=${category}&apiKey=${apiKey}`)
       .then((response) => {
         setArticles(response.data.articles);
       })
